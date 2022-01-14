f528feb71703da52ac61b9db0971ce2d72465a77
ahmadou5 <78315869+ahmadou5@users.noreply.github.com>
2022-01-14 23:28:52 +0100

    Create farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -9,7 +9,7 @@ const farms: SerializedFarmConfig[] = [
    */
   {
     pid: 0,
-    lpSymbol: 'CAKE',
+    lpSymbol: 'VoFi',
     lpAddresses: {
       97: '0x9C21123D94b93361a29B2C2EFB3d5CD8B17e0A9e',
       56: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
@@ -19,7 +19,7 @@ const farms: SerializedFarmConfig[] = [
   },
   {
     pid: 251,
-    lpSymbol: 'CAKE-BNB LP',
+    lpSymbol: 'VoFi-BNB LP',
     lpAddresses: {
       97: '0x3ed8936cAFDF85cfDBa29Fbe5940A5b0524824F4',
       56: '0x0eD7e52944161450477ee417DE9Cd3a859b14fD0',
